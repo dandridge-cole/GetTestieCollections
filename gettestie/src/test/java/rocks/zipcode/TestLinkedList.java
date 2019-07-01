a3d33fc4deca96571d89cc72186aa9dec17f9479
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-01 14:39:49 -0400

    Linked LIst and Priority Queue

--- gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
+++ gettestie/src/test/java/rocks/zipcode/TestLinkedList.java
@@ -1,4 +1,87 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.*;
+
 public class TestLinkedList {
+
+    @Test
+    public void LinkedListConstructorFromColSizeTest(){
+        List<Person> aL = new ArrayList<>();
+        Person p1 = new Person("John Doe",1111);
+        Person p2 = new Person("Jawn D'oh",1222);
+        Person p3 = new Person("Jane Dai",1333);
+        aL.add(p1);
+        aL.add(p2);
+        aL.add(p3);
+        List<Person> lL = new LinkedList<>(aL);
+        int actual = lL.size();
+        int expected = 3;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void linkedListConstructorFromColContainsTest(){
+        List<Person> aL = new ArrayList<>();
+        Person p1 = new Person("John Doe",1111);
+        Person p2 = new Person("Jawn D'oh",1222);
+        Person p3 = new Person("Jane Dai",1333);
+        aL.add(p1);
+        aL.add(p2);
+        aL.add(p3);
+        List<Person> lL = new LinkedList<>(aL);
+        Boolean containsAll = true;
+        for(Person p:aL) if(!lL.contains(p))containsAll=false;
+        Assert.assertTrue(containsAll);
+    }
+
+
+    @Test
+    public void linkedLstInsertionOrderTest(){
+        List<Person> aL = new ArrayList<>();
+        aL.clear();
+        Person p1 = new Person("One",1111);
+        Person p2 = new Person("Two",1222);
+        Person p3 = new Person("Three",1333);
+        Person p4 = new Person("Four",1444);
+        aL.add(p1);
+        aL.add(p2);
+        aL.add(p3);
+        aL.add(p4);
+        List<Person> lL = new LinkedList<>(aL);
+        String expected = aL.toString();
+        String actual = lL.toString();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void linkedLst(){
+        List<Person> aL = new ArrayList<>();
+        aL.clear();
+        List<Person> aL2 = new ArrayList<>();
+        aL2.clear();
+        Person p0 = new Person("Zero",1000);
+        Person p1 = new Person("One",1111);
+        Person p2 = new Person("Two",1222);
+        Person p3 = new Person("Three",1333);
+        Person p4 = new Person("Four",1444);
+        aL.add(p0);
+        aL.add(p2);
+        aL.add(p3);
+        aL.add(p4);
+        aL2.add(p0);
+        aL2.add(p1);
+        aL2.add(p2);
+        aL2.add(p3);
+        aL2.add(p4);
+        List<Person> lL = new LinkedList<>(aL);
+        String expected = aL2.toString();
+        lL.add(1, p1);
+        String actual = lL.toString();
+        Assert.assertEquals(expected,actual);
+    }
+
+
 }
--- gettestie/src/test/java/rocks/zipcode/TestPriorityQueue.java
+++ gettestie/src/test/java/rocks/zipcode/TestPriorityQueue.java
@@ -1,4 +1,25 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.PriorityQueue;
+
 public class TestPriorityQueue {
+
+    @Test
+
+    public void priorityQueueTestAddAndSize(){
+        Address a1 = new Address("Street","Town","Postal Code","Country");
+        Address a2 = new Address("Street","Town","Postal Code","That Country");
+        Address a3 = new Address("Street","Town","Postal Code","This Country");
+        Address a4 = null;
+        PriorityQueue pQ = new PriorityQueue();
+        pQ.add(a1);
+        pQ.add(a3);
+        pQ.add(a2);
+        int expected = 3;
+        int actual = pQ.size();
+        Assert.assertEquals(expected,actual);
+    }
 }
